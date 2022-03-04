41e9550d585d897a77f8a93f007248e0c0df914f
unknown <5somas01@solent.ac.uk>
2022-03-04 01:33:27 +0000

    lastreview

---
+++ review1.py
@@ -0,0 +1,12 @@
+print("please enter the number of lives")
+lives=int(input())
+print("please enter the energy level.")
+energy=int(input())
+print("please enter the shield level.")
+shield=int(input())
+
+print("Health has been set.")
+
+print(f"lives:{lives*'♥'}")
+print(f"Energy :{energy*'♦'}")
+print(f"Shield:{shield*'♦'}")
