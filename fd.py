360db2330679e03428efe0957c88aab5df4362f8
srs09 <99892013+srs09@users.noreply.github.com>
2022-03-11 20:14:18 +0000

    Add files via upload

---
+++ fd.py
@@ -0,0 +1,74 @@
+# print("\nDisplay a new line")
+# print("\tDisplays a tab space")
+# print("Displays a back slash")
+# print("\" Dsplays a double quote")
+# print("display backslash")
+# print("\"display a double quote")
+# print("\'displays a single quote")
+
+
+# 1.print("what is your name")
+# #ame = input()
+# print(f"here is your name {name}")
+
+
+# 2.name = input("what is your name\n")
+#
+# print(f"here is your name:\n{name}")
+
+
+# 3. print("please enter a character for the eye")
+# exp = str(input())
+# print("Beeps expression is now as follows:")
+# print("##########")
+# print(f"#  {exp}  {exp}  #")
+# print("#  ----  #")
+# print("##########")
+
+
+#4.print("what is your name human")
+# name = str(input())
+#
+# print("how old are you(in years)?")
+# old = int(input())
+#
+# print("how tall are you(in meters)?")
+# tall = float(input())
+#
+# print("how much do you weigh(in kilograms)?")
+# weigh = int(input())
+# bmi = weigh / (tall**2)
+# print(f"{name} you are {old} and your bmi is {bmi} ")
+
+
+
+# 5.print("please enter the number of lives")
+# lives = int(input())
+#
+# print("please enter the energy level.")
+# energy = int(input())
+#
+# print("please enter the shield level")
+# shield = int(input())
+# print("Health has been set")
+#
+# print(f"lives:{lives*'♥'}")
+# print(f"Energy:{energy*'♦'}")
+# print(f"Shield:{shield*'♦'}")
+
+
+print("enter the first number")
+first_number = int(input())
+
+print("enter the second number")
+second_number = int(input())
+
+
+if first_number > second_number:
+    print("the first number is bigger")
+
+
+else:
+    print("second number is bigger")
+
+
